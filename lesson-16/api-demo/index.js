77ea217d1b2666d6b4e9dded8f35a5934d222f1f
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-01-21 05:59:01 +0100

    démo leçon 16

--- lesson-16/api-demo/index.js
+++ lesson-16/api-demo/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", function (request, response) {
-    response.send("contact api");
+const contacts = [
+    { id: 1, firstName: "Ronald", lastName: "Almonce", email: "ronald.almonce@mail.com" },
+    { id: 2, firstName: "Sonia", lastName: "Cornweill", email: "sonai.cornweill@mail.com" },
+    { id: 3, firstName: "Zigfrid", lastName: "Vondeur", email: "zigvon@other.site" },
+    { id: 4, firstName: "Camille", lastName: "Engels", email: "camille.engels@rep.be" }
+];
+
+app.use(express.static("public"));
+
+app.get("/contacts", function (request, response) {
+    return response.json(contacts);
 });
 
 app.listen(port, () => console.log(`prêt sur http://localhost:3000`));
