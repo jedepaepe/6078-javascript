0a4316872517c486bd30f9557a9145bb801ca1a8
jedepaepe <jedepaepe@epfc.eu>
2023-03-15 20:52:52 +0100

    implement PUT & DELETE

--- lesson-22/index.js
+++ lesson-22/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 async function main() {
-    const uri = "mongodb+srv://6078-javascript:<<password>>@<<your-cluster>>.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://6078-javascript:u3kQhWyNnk6kC9Ff@cluster0.vgckisb.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     await client.connect();
     console.log("db connected");
@@ -39,7 +39,28 @@ async function main() {
         } catch (error) {
             response.status(500).send(error);
         }
-    })
+    });
+
+    app.put("/personel/:id", async (request, response) => {
+        try {
+            const result = await collection.updateOne(
+                { _id: new ObjectId(request.params.id) },
+                { $set: request.body }
+            );
+            response.json(result);
+        } catch (error) {
+            response.status(500).send(error);
+        }
+    });
+
+    app.delete("/personel/:id", async (request, response) => {
+        try {
+            const result = await collection.deleteOne({ _id: new ObjectId(request.params.id) });
+            response.json(result);
+        } catch (error) {
+            response.status(500).send(error);
+        }
+    });
 
     const port = 3000;
     app.listen(port, () => console.log(`ready on http://localhost:${port}`));
